e857d26c1e91220e3db9032440535ec53438b752
CristinaEU1 <cristina.elena.u@gmail.com>
2025-12-03 13:10:29 +0200

    commit 01

--- app/src/main/java/com/db/training/pokedex/ui/PokemonViewModel.kt
+++ app/src/main/java/com/db/training/pokedex/ui/PokemonViewModel.kt
@@ -1,4 +1,23 @@
 package com.db.training.pokedex.ui
 
-class PokemonViewModel {
+import androidx.lifecycle.ViewModel
+import com.db.training.pokedex.data.PokemonRepositoryImpl
+import com.db.training.pokedex.domain.Pokemon
+import com.db.training.pokedex.domain.PokemonRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class PokemonViewModel : ViewModel() {
+    val repository : PokemonRepository= PokemonRepositoryImpl()
+
+    private val _pokemonList = MutableStateFlow<List<Pokemon>>(listOf())
+    val pokemonList = _pokemonList.asStateFlow()
+
+    init {
+        _pokemonList.value = repository.getPokemon()
+
+    }
+
 }
+
+
